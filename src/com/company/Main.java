87aa53812ac129fc9137954abee308828227e0b1
Admin <mojemail@onet.pl>
2020-11-27 16:12:23 +0100

    Dowolna wiadomoscxd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,30 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        int tablica[] = {2, 4, 7, 8, 3};
-
-
+    public static void Zamiana(int tablica[], int a, int b) {
+        int c = tablica[a];
+        tablica[a] = tablica[b];
+        tablica[b] = c;
 
     }
+
+
+    public static void wypisywanieElementu(int tablica[]){
+        for(int i = 0; i < 5; ++i) {
+            System.out.print(tablica[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int tablica[] = {2, 6, 3, 7, 8};
+        wypisywanieElementu(tablica);
+        System.out.print(" ");
+        Zamiana(tablica, 0, 3);
+        wypisywanieElementu(tablica);
+    }
+
+
 }
+
